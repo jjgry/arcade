45ba99b386f400431057afe06de69587df1ed804
John-Joseph Gray <jjofthegray@gmail.com>
2019-11-30 22:55:54 +0000

    Wave is now doing a lot of the work SpaceInvaders was
    Moving logic for initialising entities to wave so that additional waves can be more easily added in the future

--- space invaders/space_invaders.py
+++ space invaders/space_invaders.py
@@ -11,66 +11,13 @@ class SpaceInvaders:
     WINDOW_WIDTH = 500
     WINDOW_HEIGHT = 500
 
-    BLOCKS_SCREEN_POS = 350
-    BLOCK_STRUCTURE = [
-        [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0],
-        [0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-        [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-        [1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-    ]
-
     def __init__(self):
-        self.player = Player(250, 450)
-        self.enemies = self.get_enemies()
-        self.blocks = self.get_blocks()
-        self.projectiles = []
+        pass
 
     @staticmethod
     def start():
         space_invaders = SpaceInvaders()
         space_invaders.run()
-
-    def get_enemies(self):
-        enemies = []
-        dimensions = (8, 4)
-        spacing = 50
-        total_width = (dimensions[0] - 1) * spacing + Enemy.WIDTH
-        left_offset = (self.WINDOW_WIDTH - total_width) / 2
-        for x in range(dimensions[0]):
-            for y in range(dimensions[1]):
-                enemies.append(Enemy(
-                    spacing*x + left_offset,
-                    spacing*y + 100))
-        return enemies
-
-    def get_blocks(self):
-        blocks = []
-        NUM_DEFENCES = 4
-        spacing = 120
-        total_width = (NUM_DEFENCES - 1) * spacing + \
-            len(self.BLOCK_STRUCTURE[0]) * Block.WIDTH
-        left_offset = (self.WINDOW_WIDTH - total_width) / 2
-        for i in range(NUM_DEFENCES):
-            # draw one defence
-            for y in range(len(self.BLOCK_STRUCTURE)):
-                for x in range(len(self.BLOCK_STRUCTURE[0])):
-                    if self.BLOCK_STRUCTURE[y][x] == 1:
-                        blocks.append(Block(
-                            left_offset + spacing*i + x*Block.WIDTH,
-                            y*Block.HEIGHT + self.BLOCKS_SCREEN_POS))
-        return blocks
 
     def run(self):
         pygame.init()
@@ -80,12 +27,11 @@ class SpaceInvaders:
         pygame.font.init()
         font = pygame.font.Font(pygame.font.get_default_font(), 20)
 
-        wave = Wave(self.blocks, self.enemies, self.player, self.projectiles,
-                    self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
+        wave = Wave(self.WINDOW_WIDTH, self.WINDOW_HEIGHT)
 
         run = True
         while run:
-            pygame.time.delay(3)  # pause for some number of ms
+            pygame.time.delay(10)  # pause for some number of ms
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -100,7 +46,7 @@ class SpaceInvaders:
             win.fill((0, 0, 0))
             wave.draw(win)
             lives_text = font.render(
-                "Lives: " + str(self.player.lives), True, (255, 255, 255))
+                "Lives: " + str(wave.player.lives), True, (255, 255, 255))
             win.blit(lives_text, (int(20), 20))
 
             # push updates to display
--- space invaders/wave.py
+++ space invaders/wave.py
@@ -7,16 +7,34 @@ from projectile import Projectile
 
 class Wave:
     """ A wave holds all of the state of the enemies, projectiles and player
-    on the screen at any given time
+    on the screen at any given time. It is also responsible for updating and 
+    drawind all of these entities whenever it is called to do so.
     """
+    EMEMIES_DIM = (6, 4)
+    ENEMIES_X_SPACING = 50
+    ENEMIES_Y_SPACING = 50
 
-    def __init__(self, blocks, enemies, player, projectiles, WINDOW_WIDTH, WINDOW_HEIGHT):
-        self.enemies = enemies
-        self.blocks = blocks
-        self.player = player
-        self.projectiles = projectiles
+    DEFENCE_SCREEN_POS = 350
+    DEFENCE_NUM = 3
+    DEFENCE_SPACING = 120
+    DEFENCE_STRUCTURE = [
+        [0, 0, 1, 1, 1, 1, 0, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [0, 1, 1, 1, 1, 1, 1, 0],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1, 1, 1],
+        [1, 1, 0, 0, 0, 0, 1, 1],
+    ]
+
+    def __init__(self, WINDOW_WIDTH, WINDOW_HEIGHT):
         self.WINDOW_WIDTH = WINDOW_WIDTH
         self.WINDOW_HEIGHT = WINDOW_HEIGHT
+        self.enemies = self.get_enemies()
+        self.blocks = self.get_blocks()
+        self.player = Player(250, 450)
+        self.projectiles = []
 
     def update(self, keys):
         """ Update all objects in the wave """
@@ -31,3 +49,41 @@ class Wave:
         Enemy.draw_all(win, self.enemies)
         self.player.draw(win)
         Projectile.draw_all(win, self.projectiles)
+
+    def get_enemies(self):
+        """ Enemies are grouped into columns. Initialise so that only the 
+        bottom-most enemy can fire.
+
+        The returned array is such that enemies[0][3] is the enemy in the 1-st
+        column and the 4-th row (1-st row is the top of screen)
+        """
+        enemies = []
+        total_width = (self.EMEMIES_DIM[0] - 1) * \
+            self.ENEMIES_X_SPACING + Enemy.WIDTH
+        left_offset = (self.WINDOW_WIDTH - total_width) / 2
+        for x in range(self.EMEMIES_DIM[0]):
+            column = []
+            for y in range(self.EMEMIES_DIM[1]):
+                column.append(Enemy(
+                    self.ENEMIES_X_SPACING * x + left_offset,
+                    self.ENEMIES_Y_SPACING * y + 100,
+                    False))
+            enemies.append(column)
+        for column in enemies:
+            column[self.EMEMIES_DIM[1] - 1].can_fire = True
+        return enemies
+
+    def get_blocks(self):
+        blocks = []
+        total_width = (self.DEFENCE_NUM - 1) * self.DEFENCE_SPACING + \
+            len(self.DEFENCE_STRUCTURE[0]) * Block.WIDTH
+        left_offset = (self.WINDOW_WIDTH - total_width) / 2
+        for i in range(self.DEFENCE_NUM):
+            # draw one defence
+            for y in range(len(self.DEFENCE_STRUCTURE)):
+                for x in range(len(self.DEFENCE_STRUCTURE[0])):
+                    if self.DEFENCE_STRUCTURE[y][x] == 1:
+                        blocks.append(Block(
+                            x * Block.WIDTH + self.DEFENCE_SPACING * i + left_offset,
+                            y * Block.HEIGHT + self.DEFENCE_SCREEN_POS))
+        return blocks
